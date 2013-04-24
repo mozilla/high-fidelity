6871a16925ae8882a2ad2d3b2fb3e943513be977
Matthew Riley MacPherson <matt@lonelyvegan.com>
2013-04-24 09:50:57 -0500

    Don't complain about build.js

--- build.js
+++ build.js
@@ -1,13 +1,6 @@
+/*jshint expr:true */
 ({
-    // baseUrl: "js",
-    // map: { '*': { 'jquery': 'zepto' } },
-    // dir: "./www-built",
-    // appDir: "./www",
-    // removeCombined: true,
     mainConfigFile: './www/js/main.js',
-    // modules: [
-    //     { name: "./main" }
-    // ],
     name: 'main',
     out: './www-built/js/main.built.js',
     optimize: 'none'
