c5e45559354d7092de01c802bfba262ae627fec2
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-05-28 10:45:49 -0400

    Add ability to query fixtures

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -1,5 +1,17 @@
 HighFidelity.ApplicationAdapter = DS.FixtureAdapter;
-// HighFidelity.ApplicationSerializer = DS.LSSerializer.extend();
-// HighFidelity.ApplicationAdapter = DS.LSAdapter.extend({
-//     namespace: 'hifi'
-// });
+
+// Add ability to query fixtures in development mode.
+if (HighFidelity.ApplicationAdapter === DS.FixtureAdapter) {
+    DS.FixtureAdapter.reopen({
+        queryFixtures: function(records, query, type) {
+            return records.filter(function(record) {
+                for (var key in query) {
+                    if (!query.hasOwnProperty(key)) { continue; }
+                    var value = query[key];
+                    if (record[key] !== value) { return false; }
+                }
+                return true;
+            });
+        }
+    });
+}
