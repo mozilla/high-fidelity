0ad984725cc84315637843e7ae55a51ae32ffcd5
Matthew Riley MacPherson <matt@lonelyvegan.com>
2014-07-31 00:43:47 -0400

    Clean up router

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,10 +3,7 @@ HighFidelity.Router.map(function() {
     this.route('search');
 
     this.resource('podcasts');
-    this.resource('podcast.new',  {path: '/podcast/new'});
-    this.resource('podcast', {path: '/podcast/:podcast_id'}, function() {
-        // this.route('episodes', {path: '/episodes'});
-        this.route('edit');
-    });
+    this.resource('podcast.new', {path: '/podcast/new'});
+    this.resource('podcast', {path: '/podcast/:podcast_id'});
     this.resource('episode', { path: '/episode/:episode_id' });
 });
