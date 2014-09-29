603a0112905f1ef7fd9bc2a4d8a77425ed45874f
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2014-09-29 17:53:05 +0200

    Add Polish (pl).
    Correct dots. If there are wrong.

--- app/scripts/l10n/translations.js
+++ app/scripts/l10n/translations.js
@@ -2,6 +2,7 @@ I18n.defaultLocale = "en-US";
 I18n.locale = window.navigator.language || 'en-US';
 I18n.fallbacks = true;
 I18n.translations = {
+    // English by @tofumatt
     en: {
         app: {
             title: 'Podcasts',
@@ -32,6 +33,7 @@ I18n.translations = {
         },
     },
 
+    // French by @AntoineTurmel
     fr: {
         app: {
             title: 'Podcasts',
@@ -59,6 +61,37 @@ I18n.translations = {
             myPodcasts: 'mes podcasts',
             popular: 'populaire',
             search: 'recherche',
+        },
+    },
+    
+    // Polish by @MaciejCzyzewski
+    pl: {
+        app: {
+            title: 'Podcasty', // Transmisje
+        },
+        episode: {
+            episodes: 'Epizody', // Fragmenty
+            new: 'Nowe:',
+        },
+        podcast: {
+            add: 'Dodaj',
+            addOne: 'Chcesz jedno dodać?',
+            addRSSFeed: 'Dodaj źródło RSS',
+            newToPodcasts: 'Nowe do podcastu?',
+            noneFound: 'Nie znaleziono podcastów.',
+            recommendations: 'Sprawdź nasze rekomendacje:',
+        },
+        search: {
+            find: 'Szukaj',
+            noResults: 'Brak wyników.',
+            placeholder: 'Podcast tytuł albo słowo kluczowe',
+            searchForAPodcast: 'Szukaj w podcastach',
+            subscribe: 'Subskrybuj {{podcast}}?',
+        },
+        tabs: {
+            myPodcasts: 'Moje Podcasty',
+            popular: 'Popularne',
+            search: 'Szukaj',
         },
     },
 };
