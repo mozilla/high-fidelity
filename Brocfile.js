591cef1a60335872c801c68f877c8c1b52a36a48
Brittany Storoz <brittanystoroz@gmail.com>
2015-07-08 10:03:28 -0400

    Stub templates and localization helper

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,8 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+app.import('bower_components/i18n-js/app/assets/javascripts/i18n.js');
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
