8e249f163349fa1a44e5bd0c590974a6784bbc89
Brittany Storoz <brittanystoroz@gmail.com>
2015-07-18 11:44:05 -0400

    Comment out check for duplicate podcasts...

--- app/controllers/podcasts/new.js
+++ app/controllers/podcasts/new.js
@@ -26,16 +26,16 @@ export default Ember.Controller.extend({
 
       self.set('isAdding', true);
 
-      var existingPodcast = self.store.find('podcast', {
-        rssURL: self.get('rssURL')
-      }).then(function(podcast) {
-        console.log('Podcast: ', podcast);
-        console.info('Podcast already exists', podcast.objectAt(0));
+      // var existingPodcast = self.store.find('podcast', {
+      //   rssURL: self.get('rssURL')
+      // }).then(function(podcast) {
+      //   console.log('Podcast: ', podcast);
+      //   console.info('Podcast already exists', podcast.objectAt(0));
 
-        self.set('isAdding', false);
-        self.set('rssURL', '');
-        self.transitionToRoute('podcast', podcast.objectAt(0));
-      }, function() {
+      //   self.set('isAdding', false);
+      //   self.set('rssURL', '');
+      //   self.transitionToRoute('podcast', podcast.objectAt(0));
+      // }, function() {
 
         console.info('Creating new podcast.');
 
@@ -52,7 +52,7 @@ export default Ember.Controller.extend({
           self.set('isAdding', false);
           self.set('isInErrorState', true);
         });
-      });
+      // });
     }
   }
 });
