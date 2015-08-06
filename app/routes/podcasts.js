2b0ce5bb1359ba1fc4b60a6a1d763648fddbdfa7
Brittany Storoz <brittanystoroz@gmail.com>
2015-08-05 23:33:06 -0400

    Fix deprecated implementations

--- app/routes/podcasts.js
+++ app/routes/podcasts.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.get('store').find('podcast');
+    return this.get('store').findAll('podcast');
   }
 });
