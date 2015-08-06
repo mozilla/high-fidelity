1a0c71448ffef03b424a92b2c33f9b992f261fcc
Brittany Storoz <brittanystoroz@gmail.com>
2015-08-06 05:50:01 -0400

    Remove Brocfile in favor of ember-cli-build
    Transition reference here:
    https://github.com/ember-cli/ember-cli/blob/master/TRANSITION.md#brocfile-transition

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    wrapInEval: false
   });
 
   // Use `app.import` to add additional libraries to the generated
