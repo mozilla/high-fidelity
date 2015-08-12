cdd28ef0a0dfd8d5087a4e8d49fa7d7cb7fb96b6
Brittany Storoz <brittanystoroz@gmail.com>
2015-08-12 02:28:56 -0400

    Fix jshint errors on locales

--- app/initializers/i18n.js
+++ app/initializers/i18n.js
@@ -1,9 +1,7 @@
 export default {
   name: 'i18n',
-
   after: 'ember-i18n',
-
   initialize: function(_, app) {
-    app.inject('model', 'i18n', 'service:i18n')
+    app.inject('model', 'i18n', 'service:i18n');
   }
 };
