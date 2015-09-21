134117fbff74e7facf8ef945fdf051a72f75064e
Brittany Storoz <brittanystoroz@gmail.com>
2015-09-21 14:10:49 -0400

    Fix initialization for i18n setting locale

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  i18n: Ember.inject.service(),
   model: function() {
       return this.store;
+  },
+  afterModel: function(user) {
+    var i18nLocales = this.get('i18n.locales');
+    var locale = (window.navigator.language || window.navigator.browserLanguage).split('-')[0];
+    this.set('i18n.locale', locale);
   },
   actions: {
     closeModal: function() {
